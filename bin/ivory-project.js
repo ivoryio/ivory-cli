ec706b7f8c6d08f868de053b21266dcb368ad51f
Bogdan Balan <bogdan.balan@thinslices.com>
2019-03-05 17:08:38 +0200

    feat(Project commands): Remove project deploy and add project configure
    BREAKING CHANGE: The deploy command doesn't exist anymore

--- bin/ivory-project.js
+++ bin/ivory-project.js
@@ -6,5 +6,5 @@ program
   .version(pkg.version)
   .description('Commands that will create, test and deploy an Ivory SPA web app')
   .command('create', 'creates the project structure of an Ivory app')
-  .command('deploy', 'creates the AWS CloudFormation stacks and configures AWS Amplify.')
+  .command('configure', 'configures the project using the given AWS credentials')
   .parse(process.argv)
