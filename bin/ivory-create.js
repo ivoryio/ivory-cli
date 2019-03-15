dd3d1076e7d9059de4213098a68722445d46ce1a
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-03-15 16:01:16 +0200

    feat(create command): Add ivory create command instead of ivory project create

--- bin/ivory-create.js
+++ bin/ivory-create.js
@@ -1,10 +1,10 @@
 const program = require('commander')
 
 const pkg = require('../package.json')
+const create = require('../lib/commands/create')
 
 program
   .version(pkg.version)
   .description('Create a new Ivory project or a new pod for an existing project')
-  .command('project', 'create a new Ivory project')
-  .command('pod', 'create a new pod for an existing Ivory project')
+  .action(() => create())
   .parse(process.argv)
