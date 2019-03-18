0b810686641452d71ec26607381ec4c8b3ea05a4
Bogdan Balan <bogdan.balan@thinslices.com>
2019-03-18 17:15:09 +0200

    chore(Command description): Update the Ivory CLI description

--- bin/ivory-cli.js
+++ bin/ivory-cli.js
@@ -5,8 +5,8 @@ const pkg = require('../package.json')
 
 program
   .version(pkg.version)
-  .description('*** ALPHA BUILD ***')
+  .description('Ivory CLI 🐘 is a tool for developing, testing and deploying SPA web applications build with the Ivory Pod Architecture (IPA).')
+  .command('create', 'command to create a new Ivory project or a new pod for an existing project')
   .command('configure', 'command to configure the project using the given AWS credentials')
-  .command('create', 'command to create a new project or a new pod for an existing project')
-  .command('deploy', 'command deploy an entire Ivory project or an Ivory Pod')
+  .command('deploy', 'command deploy the entire project or a pod microservice')
   .parse(process.argv)
