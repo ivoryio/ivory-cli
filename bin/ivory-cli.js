641e53ad75ab0edfa73f2ef1ba47d068059f3cee
Bogdan Balan <bogdan.balan@thinslices.com>
2019-06-22 21:34:11 +0300

    Remove all unnecesary commands
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

--- bin/ivory-cli.js
+++ bin/ivory-cli.js
@@ -5,10 +5,7 @@ const pkg = require('../package.json')
 
 program
   .version(pkg.version)
-  .description('Ivory CLI 🐘 is a tool for developing, testing and deploying SPA web applications build with the Ivory Pod Architecture (IPA).')
-  .command('create', 'command to create a new Ivory project or a new pod for an existing project')
-  .command('configure', 'command to configure the project using the given AWS credentials')
-  .command('deploy', 'command deploy the entire project or a pod microservice')
-  .command('init', 'command to install all dependencies for an existing project')
-  .command('nuke', 'command to delete an Ivory pod or the infrastructure of a project')
+  .description('Ivory CLI 🐘 is a tool for developing, testing and deploying web applications build with the Ivory Architecture (IA).')
+  .command('create', 'command to create a new Ivory project based on a template')
+  .command('deploy', 'command to deploy the CI/CD piplines for the project')
   .parse(process.argv)
--- bin/ivory-configure.js
+++
@@ -1,9 +0,0 @@
-const program = require('commander')
-
-const pkg = require('../package.json')
-const configure = require('../lib/commands/configure')
-
-program
-  .version(pkg.version)
-  .action(() => configure())
-  .parse(process.argv)
