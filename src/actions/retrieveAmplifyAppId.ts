d9b3d2dee492b5393a1e791533ce946d16b06566
Bogdan Balan <bogdan.balan@thinslices.com>
2020-05-19 11:02:31 +0300

    fix comment :))
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

--- src/actions/retrieveAmplifyAppId.ts
+++ src/actions/retrieveAmplifyAppId.ts
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk'
 export const retrieveAmplifyAppId = async ({ awsProfile }: AppConfiguration) => {
   process.env.AWS_PROFILE = awsProfile
 
-  // just get region from AppConfiguration and inquire the region from the user
+  // should get region from AppConfiguration and inquire the region from the user
   const cf = new AWS.CloudFormation({ region: 'eu-central-1' })
 
   const response = await cf.listExports().promise()
