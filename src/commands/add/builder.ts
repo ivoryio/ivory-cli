b4d538ab16caae5478cf8ae1eb140cc46ef6fc8f
Bogdan Balan <bogdan.m.balan@gmail.com>
2020-05-20 14:40:44 +0300

    add stubs for add command

---
+++ src/commands/add/builder.ts
@@ -0,0 +1,3 @@
+import { add as buildAddCommand } from './command'
+
+export const add = buildAddCommand()
---
+++ src/commands/add/command.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const add = (/*dependencies: any */) => (/* input_data : any */) => {}
