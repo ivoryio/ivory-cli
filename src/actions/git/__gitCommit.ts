7fc30927304a6043da08b814836adefadcf7ae5d
Lucian Vasiliu <lucian@thinslices.com>
2020-05-25 10:53:28 +0300

    Fix for uncommitted files

--- src/actions/git/__gitCommit.ts
+++ src/actions/git/__gitCommit.ts
@@ -2,7 +2,8 @@ import shell from 'shelljs'
 
 export const gitCommitAll = (commitMessage: string) => {
   return new Promise((resolve, reject) => {
-    shell.exec(`git commit -am "${commitMessage}"`, (code, _, stderr) => {
+    shell.exec(`git add *`)
+    shell.exec(`git commit -m "${commitMessage}"`, (code, _, stderr) => {
       if (code !== 0) {
         reject(stderr)
       } else {
